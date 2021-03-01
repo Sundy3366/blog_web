eacb1146beb76a6f23a60de56c685622685e4712
xixi <511233380@qq.com>
2021-03-01 20:02:29 +0800

    feature: 评论

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {getArticle} from '@request'
-import {Button, Avatar} from 'antd';
+import {Button, Avatar, Input} from 'antd';
 import history from '@history'
 import styles from '@css/article/article.module.scss'
 import Comment from '@components/article/Comment'
-
+const { TextArea } = Input;
 const Article = (props) => {
     const {articleId} = props.match.params
     const [article, setArticle] = useState(null)
@@ -32,14 +32,30 @@ const Article = (props) => {
                     <div className={styles.author}>
                         <Avatar
                             size={60}
-                            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
+                            src="https://upload.jianshu.io/users/upload_avatars/15884256/e58a844d-08f8-47b4-b947-79818093d797?imageMogr2/auto-orient/strip|imageView2/1/w/100/h/100/format/webp"/>
                         <div className={styles.desc}>
                             <div>不忘初心_6b23</div>
                             <div>总资产4 (约0.36元)共写了9624字获得45个赞共22个粉丝</div>
                         </div>
                     </div>
+                    <div>
+                        <div className={styles.myComment}>
+                            <Avatar
+                                size={60}
+                                src="https://upload.jianshu.io/users/upload_avatars/15884256/e58a844d-08f8-47b4-b947-79818093d797?imageMogr2/auto-orient/strip|imageView2/1/w/100/h/100/format/webp"/>
+                            <TextArea placeholder="textarea with clear icon" allowClear/>
+                        </div>
+                        <Button htmlType="submit"  type="primary" style={{margin: '20px 60px'}}>
+                           发布
+                        </Button>
+                    </div>
+
                     <div className={styles.comment}>
-                        <Comment/>
+                        <Comment>
+                            <Comment>
+                                <Comment/>
+                            </Comment>
+                        </Comment>
                     </div>
                 </div>
 
